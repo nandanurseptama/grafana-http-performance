7acfa8f97d6363b56a654b246b974e4b847681e4
nandanurseptama <nandanurseptama@gmail.com>
2025-04-22 08:14:40 +0700

    chore: add rust to grafana

--- k6/base.js
+++ k6/base.js
@@ -1,11 +1,7 @@
 export const baseOptions = {
   stages: [
     { duration: "30s", target: 20 },
-    { duration: "1m30s", target: 30 },
-    { duration: "1m30s", target: 40 },
-    { duration: "3m", target: 100 },
-    { duration: "1m30s", target: 30 },
-    { duration: "1m", target: 10 },
+    { duration: "1m", target: 30 },
     { duration: "30s", target: 0 },
   ],
 };
